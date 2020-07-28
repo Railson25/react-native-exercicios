9229868dad8b32b3fb2b7e623b03fdb8ef34a3fa
Railson25 <railsonsantiago123@gmail.com>
2020-07-28 10:34:59 -0300

    Import/Export

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { View } from 'react-native'
 
 import Primeiro from './components/Primeiro'
+import Comp, { Comp1, Comp2 } from './components/Multi'
 
 
 export default () => (
     <View>
+        <Comp />
+        <Comp1 />
+        <Comp2 />
         <Primeiro />
     </View>
 )    
---
+++ exercicios/src/components/Multi.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function Comp() {
+    return <Text> Componente</Text>
+}
+
+export function Comp1() {
+    return <Text>Comp 1</Text>
+}
+
+ function Comp2() {
+    return <Text>Comp 2</Text>
+}
+
+export { Comp2 }
