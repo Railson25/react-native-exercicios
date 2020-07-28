05e2ca76b3f0a8d7b944306210888ebdec323a46
Railson25 <railsonsantiago123@gmail.com>
2020-07-28 10:24:24 -0300

    Organizando Componente

--- exercicios/src/App.js
+++ exercicios/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { View } from 'react-native'
 
-function App() {
-    return <Text>
-        Primeiro Componente 
-    </Text>
-}
+import Primeiro from './components/Primeiro'
 
-export default App
+
+export default () => (
+    <View>
+        <Primeiro />
+    </View>
+)    
