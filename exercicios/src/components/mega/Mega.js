31687b8f8a4318231b6298beb3724d795b7fe64e
Railson25 <railsonsantiago123@gmail.com>
2020-07-29 13:15:11 -0300

    Componente classe: Passando Props

--- exercicios/src/components/mega/Mega.js
+++ exercicios/src/components/mega/Mega.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react'
 import { Text } from 'react-native'
 import Style from '../estilo'
 
-
-
 export default class Mega extends Component {
+
     render(){
         return(
-            <Text style={Style.font}> Componente Classe</Text>
+            <Text style={Style.font}> 
+                Componente Classe {this.props.qtdeNumeros}
+            </Text>
         )
     }
 }
